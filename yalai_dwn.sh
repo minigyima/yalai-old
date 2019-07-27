249a6dc8f3fd53ef1c4206be6e3ce63f7fb77032
minigyima <35065974+minigyima@users.noreply.github.com>
2019-07-27 08:21:17 +0200

    Downloader ver. 1.2

--- yalai_dwn.sh
+++ yalai_dwn.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # YaLAI downloader script
-# Version 1.1
+# Version 1.2
 # Written by minigyima
 # Copyright 2019
 
 title="YaLAI Downloader"
-wellcome_text="Welcome to YaLAI (Yet another Live Arch Installer)! \nPlease ensure that you have an active connection to de internet, since it is mandatory for downloading this installer and also for the installation of Arch Linux. If you need to connect to a wifi network, you may click the NetworkManager icon in the top left corner of the panel. However, if you are using a wireless connection, NetworkManager should automatically detect that, and configure it for you.\nOnce you are connected to the internet, click the 'Yes' button on this dialogbox to proceed. If you would like to exit the installer, press the 'No' button instead.\nAlso you can use this live installer as a rescue cd, by opening a terminal from the system tools menu."
+wellcome_text="Welcome to YaLAI (Yet another Live Arch Installer)! \nPlease ensure that you have an active connection to the internet, since it is mandatory for downloading the installer and also for the installation of Arch Linux. If you need to connect to a wifi network, you may click the NetworkManager icon in the top right corner of the panel. However, if you are using a wired connection, NetworkManager should automatically detect that, and configure it for you.\nOnce you are connected to the internet, click the 'Yes' button on this dialogbox to proceed. If you would like to exit the installer, press the 'No' button instead.\nAlso you can use this live installer as a rescue cd, by opening a terminal from the system tools menu."
 
 # Displaying welcome message
 welcome_box () { 
@@ -38,13 +38,11 @@ installer() {
 		if [ "$choice" = "Restart" ]
 		then reboot
 		elif [ "$choice" = "Chroot into new system" ]
-		then sudo mount $(cat root_part.txt) /mnt
-		mate-terminal -e sudo arch-chroot /mnt
-		umount -R /mnt
-		elif [ "$choice" = "Open pacman.conf" ]
-		then sudo mount $(cat root_part.txt) /mnt
-		mate-terminal -e sudo nano /mnt/etc/pacman.conf     
-		else exit
+		then mate-terminal -e "arch-chroot /mnt" -t "Chroot"
+		elif [ "$choice" = "Open pacman.conf in nano" ]
+		then mate-terminal -e "nano /mnt/etc/pacman.conf"     
+		else umount /mnt
+		exit
 		fi
 }
 
